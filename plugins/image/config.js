61ad03f3e14f3f6e3c7788b6f643adcd09b712bc
= <=>
2024-07-26 15:43:45 +0900

    load the sub .env file and use the env variable for strange api key

--- plugins/image/config.js
+++ plugins/image/config.js
@@ -1,7 +1,8 @@
 const { Config } = require("strange-sdk");
+require("dotenv").config({path: require("path").join(__dirname, ".env")});
 
 module.exports = new Config(__dirname, {
     EMBED_COLOR: "#36393F",
     STRANGE_API_URL: "https://strangeapi.hostz.me/api",
-    STRANGE_API_KEY: "YOUR_API_KEY",
+    STRANGE_API_KEY: process.env.STRANGE_API_KEY,
 });
